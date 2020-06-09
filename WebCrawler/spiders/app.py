ec27e0988fbdcacea8d03c0a4c037b84c900cd0c
DADI91 <48893991+DADI91@users.noreply.github.com>
2020-06-09 18:04:37 +0200

    projet_scrap

---
+++ WebCrawler/spiders/app.py
@@ -0,0 +1,13 @@
+import requests
+import scrapy
+
+
+class maisonSpider(scrapy.Spider):
+    name = 'seloger'
+    start_urls = "https://www.seloger.com/list.htm?projects=2,5&types=1,2&natures=1,2,4&places=[{cp:75}]&price=NaN/1200&rooms=3&enterprise=0&qsVersion=1.0"
+
+    def parse(self, response):
+        for title in response.css('.post-header>h2'):
+            yield {'title': title.css('a ::text').get()}
+
+        
